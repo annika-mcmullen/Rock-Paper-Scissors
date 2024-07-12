af4afa39fca28bb1130fa8a3c91944a2c8f1729e
Annika McMullen <annikamcmullen@icloud.com>
2024-07-12 13:50:57 -0500

    Created funtions for basic r.p.s. need to add logic to choose winner

--- javascript.js
+++ javascript.js
@@ -1,4 +1,36 @@
 // JavaScript for Rock Paper Scissors
 //Annika McMullen
 
-console.log("Hello World");
+//global variables
+let humanScore =0;
+let computerScore =0;
+
+//Limit computers random choices to 3 integers between 1 and 3 (inclusive)
+function getComputerChoice(){
+    let compChoice= Math.floor((Math.random() *3))+1;
+   return compChoice;
+}
+
+//Prompt user for their choice and store
+function getHumanChoice(){
+    let humanChoice = (Number(prompt("Please enter your move choice")))
+    return humanChoice
+}
+//Display a a welcome menu
+function displayMenu(){
+    console.log("**********************");
+    console.log("*Welcome to ROCK, *");
+    console.log("*PAPER, *");
+    console.log("*SCISSORS! *");
+    console.log("**********************");
+    console.log("As you play you will enter 1 for rock, 2 for paper, or 3 for scissors!");
+
+}
+
+function playRound(){
+let userChoice = getHumanChoice();
+let compChoice = getComputerChoice();
+console.log(userChoice);
+console.log(compChoice);
+}
+playRound();
